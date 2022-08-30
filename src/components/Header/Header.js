b80b654107dd5822d979f7107fafa238ec8392da
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-08-30 14:46:10 +0100

    Create Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="Header-class">I am the Header</h1>
+      <img
+        src="https://via.placeholder.com/1070x200.png"
+        alt="placeholder imsge"
+      />
+    </div>
+  );
+};
+
+export default Header;
